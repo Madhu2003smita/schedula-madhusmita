09f8d1dbdea88613f1355d8cb78d31c4cdf94f6e
madhusmita <madhusmitabarsa19@gmail.com>
2026-07-30 15:51:01 +0530

    chore: configure Railway PostgreSQL connection

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,22 +4,41 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService],
+    //   useFactory: (config: ConfigService) => ({
+    //     type: 'postgres',
+    //     host: config.get<string>('DB_HOST', 'localhost'),
+    //     port: config.get<number>('DB_PORT', 5432),
+    //     username: config.get<string>('DB_USERNAME', 'postgres'),
+    //     password: config.get<string>('DB_PASSWORD', 'madhu1928'),
+    //     database: config.get<string>('DB_NAME', 'schedula'),
+    //     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    //     migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+    //     synchronize: false,
+    //     logging: false,
+    //   }),
+    // }),
+
+
+
+
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get<string>('DB_HOST', 'localhost'),
-        port: config.get<number>('DB_PORT', 5432),
-        username: config.get<string>('DB_USERNAME', 'postgres'),
-        password: config.get<string>('DB_PASSWORD', 'madhu1928'),
-        database: config.get<string>('DB_NAME', 'schedula'),
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        migrations: [__dirname + '/../migrations/*{.ts,.js}'],
-        synchronize: false,
-        logging: false,
-      }),
-    }),
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (config: ConfigService) => ({
+    type: 'postgres',
+    url: config.get<string>('DATABASE_URL'),
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+    synchronize: false,
+    logging: false,
+  }),
+}),
   ],
 })
 export class DatabaseModule {}
